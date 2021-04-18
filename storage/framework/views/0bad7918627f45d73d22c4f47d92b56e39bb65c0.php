24fdd6e291480992a2e1ff640c4ce4acc3103e58
panshenkowski <75529452+panshenkowski@users.noreply.github.com>
2021-04-19 00:53:46 +0500

    Add files via upload

---
+++ storage/framework/views/0bad7918627f45d73d22c4f47d92b56e39bb65c0.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .container {
+            padding: 80px 120px;
+        }
+
+    </style>
+</head>
+<body>
+<h1>hallo!!!</h1>
+<p> <?php echo e($name); ?></p>
+<p> <?php echo e($email); ?></p>
+<p> <?php echo e($sms); ?></p>
+</body>
+</html>
+<?php /**PATH /Users/kadyrovkuanysh/Desktop/news_p/resources/views/mail.blade.php ENDPATH**/ ?>
